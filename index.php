042a98b1e5c23aa44dcbe8914860ee7e1a1b9ad0
manfred81 <igor_travkin@mail.ru>
2022-09-26 16:47:12 +0300

    dz3

--- index.php
+++ index.php
@@ -51,6 +51,7 @@
 // }
 // echo $out;
 // echo "Всего займет $sum ч.";
+
 $arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 $arr2 = range(1, 10);
 $result = [];
@@ -59,3 +60,22 @@ for ($i = 0; $i < count($arr1); $i++) {
     $result[] = $arr1[$i] * $arr2[$i];
 }
 print_r($result);
+
+
+
+
+$wishes = ['счастья', 'здоровья', 'удачи', 'счастья', 'радости'];
+$epithets = ['огромного', 'отличного', 'безграничного', 'крепчайщего', 'небывалого'];
+
+$name = readline('Имя именинника?');
+$wishesLimit = 3;
+$wishesChosen = [];
+for ($i = 1; $i <= $wishesLimit; $i++) {
+    $wishesChosen[] = $epithets[array_rand($epithets)] . " " . $wishes[array_rand($wishes)];
+}
+
+$lastWish = array_pop($wishesChosen);
+
+$congratulations = " <<Дорогой $name, поздравляю, и желаю тебе " . implode(', ', $wishesChosen) . " и " . $lastWish . ">>.";
+
+echo $congratulations;
