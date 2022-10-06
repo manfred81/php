6c2732b229e187fe274690bc8093c0aa7b2854fe
manfred81 <igor_travkin@mail.ru>
2022-10-07 00:06:53 +0300

    dz6

--- TaskService.php
+++
@@ -1,9 +0,0 @@
-<?php
-class TaskService
-{
-    public static function addComment(User $author, Task $task, $text): void
-
-    {
-        $task->setComment(new Comment($author, $task, $text));
-    }
-}
